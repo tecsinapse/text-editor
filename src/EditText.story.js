74aa32de15ef49d5cb296a7c343ff610159a557f
Denner Stevens <denner.vidal@gmail.com>
2019-11-28 13:53:54 -0400

    Removed ui-kit code

--- src/EditText.story.js
+++ src/EditText.story.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import { DivFlex } from '@tecsinapse/ui-kit/build/withFlexCenter';
 import { EditText } from './EditText';
-import { DivFlex } from '../withFlexCenter';
 
-storiesOf(`EditText`, module)
+storiesOf(`Text Editor`, module)
   .addDecorator(story => <DivFlex>{story()}</DivFlex>)
-  .add('EditText', () => (
+  .add('Editor', () => (
     <div style={{ width: '700px', height: '500px' }}>
       <EditText onChange={e => e /* console.log(e) */} error="" />
     </div>
