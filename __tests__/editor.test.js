0b42fffc999dc8df62e88641ca7502243f421103
Denner Stevens <denner.vidal@gmail.com>
2019-12-05 17:17:27 -0400

    Adding new instructions how to setup editor (with css)

---
+++ __tests__/editor.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { EditText } from '../src/EditText';
+
+test('Render Editor', () => {
+  const { container } = render(
+    <EditText
+      onChange={e => e}
+      error="Erro no editor"
+      skinUrl="/text-editor/oxide"
+    />
+  );
+  const errorMessage = container.querySelector('p');
+  const textArea = container.querySelector('textarea');
+  fireEvent.change(textArea, { target: { value: 'Teste de componente' } });
+  expect(errorMessage.firstChild.textContent).toBe('Erro no editor');
+  expect(textArea.value).toEqual('Teste de componente');
+});
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['./__tests__'],
+  displayName: 'editor-tests',
+  testMatch: ['**/__tests__/**/*.js'],
+  testURL: 'http://localhost',
+  transformIgnorePatterns: ['/node_modules/(?!@tecsinapse/ui-kit).+\\.js$'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+};
